68d6c867a4e09e6c9a7871c7e34bab1485753630
Hamza Mughal <66194492+hamzaamughal@users.noreply.github.com>
2021-09-22 16:25:36 +0500

    card component completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { ThemeProvider } from 'styled-components';
+import Card from './components/Card';
 import Header from './components/Header';
 import { Container } from './components/styles/Container.styled'
 import GlobalStyles from './components/styles/Global';
+import content from './content'
 
 const theme = {
   colors: {
@@ -19,7 +21,9 @@ function App() {
         <GlobalStyles />
         <Header />
         <Container>
-          <h1>Hello World</h1>
+          {content.map((item, index) => {
+            return <Card key={index} item={item} />
+          })}
         </Container>
       </>
     </ThemeProvider>
